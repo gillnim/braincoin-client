aab11fbaa266b269f436411a5c66c582ad125115
Nimrat Gill <nimratgill@Nimrats-MBP.lan>
2024-10-03 12:35:24 -0700

    fix inputs for conversion

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.scss'
+import { Toaster } from "sonner";
 
 import BalancePage from './pages/BalancePage/BalancePage.jsx'
 import TopTenPage from './pages/TopTenPage/TopTenPage';
+import CurrencyConverter from './pages/CurrencyConvertorPage/CurrencyConvertorPage.jsx';
 
 function App() {
   return (
     <BrowserRouter>
+    <Toaster richColors position="top-right" /> 
+    <CurrencyConverter />
     <Routes>
       <Route path = "/balance" element={<BalancePage/>}/>
       <Route path="/top-ten" element={<TopTenPage />} />
